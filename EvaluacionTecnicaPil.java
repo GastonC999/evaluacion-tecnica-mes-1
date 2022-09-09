950131b76c042b050ef8fc8f198cad02b2387de2
Gaston Castro <gastonc08gec@gmail.com>
2022-09-09 15:59:50 -0300

    ultima actualizacion

--- EvaluacionTecnicaPil.java
+++ EvaluacionTecnicaPil.java
@@ -28,11 +28,25 @@ public class EvaluacionTecnicaPil {
 
         imprimirConsigna(1);
 
-        // Desarrollo de la consigna 1.
+        int a = 10, b = 2, suma = a + b;
+        double c = 3.4, multiplicacion = c * b;
+
+
+        if (suma > 0) {
+            JOptionPane.showMessageDialog(null, "resultado de suma: " + suma);
+            JOptionPane.showMessageDialog(null, "resultado de multiplicacion: " + multiplicacion);
+        } else {
+            JOptionPane.showMessageDialog(null, "Error");
+        }
 
         imprimirConsigna(2);
 
-        // Desarrollo de la consigna 2.
+        System.out.println("suma = " + suma);
+        while (suma == 24){
+            suma++;
+        }
+        JOptionPane.showMessageDialog(null, "El doble de 12 es " + suma);
+
 
         imprimirConsigna(3);
 
