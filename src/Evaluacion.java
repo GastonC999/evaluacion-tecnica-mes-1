de989538253feb21d2c7d82779a5a30288443f7d
Gaston Castro <gastonc08gec@gmail.com>
2022-09-09 15:52:44 -0300

    actualizado act b

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -2,21 +2,26 @@ import javax.swing.*;
 
 public class Evaluacion {
     public static void main(String[] args) {
-        int a = 12, b = 2, suma = a + b;
+        int a = 10, b = 2, suma = a + b;
         double c = 3.4, multiplicacion = c * b;
 
 
         ////////////////
 
-        if (suma > 0) {
+/*        if (suma > 0) {
             JOptionPane.showMessageDialog(null, "resultado de suma: " + suma);
             JOptionPane.showMessageDialog(null, "resultado de multiplicacion: " + multiplicacion);
         } else {
             JOptionPane.showMessageDialog(null, "Error");
+        }*/
+
+        System.out.println("suma = " + suma);
+        while (suma == 24){
+            suma++;
         }
-        while ()
+        JOptionPane.showMessageDialog(null, "El doble de 12 es " + suma);
 
-
+        // c. Iteración --> Realizá N cantidad de impresiones, siendo N un tercio del valor de la variable del punto 'b'.
 
     }
 }
